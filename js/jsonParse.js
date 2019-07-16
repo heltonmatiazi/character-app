c6a27db84c27a1fc10215d6d9e5100a9e2486bf8
adam <adam9k@gmail.com>
2019-07-16 18:09:03 -0300

    Melhoramento na manipulação de templates

--- js/jsonParse.js
+++ js/jsonParse.js
@@ -10,19 +10,20 @@ $(document).ready(function(){
         },
         success:function(data){
             $(data.characters).each(function(index,value){
-                var record ="<tr><td>"+value.name+
-                                "</td><td>"+value.level+"</td><td>"+value.health+"</td>"+
-                                "<td>"+
-                                        "<button type='button' class='character-edit btn btn-default' data-id=' " +value.id+ " '>"+
-                                        "<span class='glyphicon glyphicon-edit'></span>"+
-                                        "</button>"+
-                                "</td>"+
-                                    "<td>"+
-                                        "<button type='button' class='character-delete btn btn-default' data-id='  " +value.id+ " '>"+
-                                            "<span class='glyphicon glyphicon-remove'></span>"+
-                                        "</button>"+
-                                    "</td>"+
-                              "<\tr>";
+                var record =$(`<tr>
+								<td>${value.name}</td>
+                                <td>${value.level}</td><td>${value.health}</td>
+                                <td>
+                                        <button type='button' class='character-edit btn btn-default' data-id='${value.id}'>
+											<span class='glyphicon glyphicon-edit'></span>
+                                        </button>
+                                </td>
+                                    <td>
+                                        <button type='button' class='character-delete btn btn-default' data-id='${value.id}'>
+                                            <span class='glyphicon glyphicon-remove'></span>
+                                        </button>
+                                    </td>
+                              <\tr>`);
                 $("#characterTableBody").append(record);
             });
         }
